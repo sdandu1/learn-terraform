43eac52b15a03dbd58b34a76939de8e6e798b173
sdandu <dsraju15@gmail.com>
2023-02-09 15:26:46 +0530

    update1

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -7,3 +7,15 @@ data "aws_ami" "example" {
 output "ami" {
   value = data.aws_ami.example
 }
+
+data "aws_instance" "foo" {
+  instance_id = "i-0f04537f6a04b9e71"
+
+}
+
+output "instance" {
+  value = "data.aws_instance.foo"
+}
+
+##i want only a public ip address #"data.aws_instance.foo.public_ip"
+
