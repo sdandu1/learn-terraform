8fd67633efa31a45e5de6537eeaebd6973bd1013
sdandu <dsraju15@gmail.com>
2023-05-29 19:29:20 +0530

    update1

---
+++ 02-data-sources/connect aws.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+output "ami" {
+  value = "data.aws_ami.example"
+}
+
+data "aws_instance" "foo" {
+  instance_id = "i-0ff88b3b75c79b701"
+}
+
+output "instance" {
+  value = "data.aws_instance.foo.public_ip"
+}
